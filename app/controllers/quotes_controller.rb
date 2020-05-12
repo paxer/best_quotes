7af6aa6e82d9b0f4f24cffd3e13c21665a6ca769
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2020-05-12 13:50:24 +1000

    update yo_ho_ho to support controllers and implement first controller

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,5 @@
+class QuotesController < YoHoHo::Controller
+  def a_quote
+    'I wish it need not have happened in my time, said Frodo. So do I, said Gandalf, and so do all who live to see such times. But that is not for them to decide. All we have to decide is what to do with the time that is given us.'
+  end
+end
---
+++ spec/controllers/quotes_controller_spec.rb
@@ -0,0 +1,14 @@
+require "rack/test"
+
+RSpec.describe QuotesController do
+  include Rack::Test::Methods
+
+  let(:app) { BestQuotes::Application.new }
+
+  context '#a_quote' do
+    it 'returns a text response' do
+      get '/quotes/a_quote'
+      expect(last_response.body).to include(' All we have to decide is what to do with the time that is given us.')
+    end
+  end
+end
